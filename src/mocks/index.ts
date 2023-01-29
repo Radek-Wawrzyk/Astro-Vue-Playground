a0cffad879824dbc67d10326f8ed9d417e4eeb7e
Radosław Wawrzyk <radekwawrzyk@gmail.com>
2023-01-29 17:15:04 +0100

    App - AppNavigation component

---
+++ src/mocks/index.ts
@@ -0,0 +1,20 @@
+import type { NavigationMenuElement } from "@/types/models/Navigation";
+
+const navigationMenu: NavigationMenuElement[] = [
+  {
+    url: '/portfolio/',
+    name: 'Portfolio',
+  },
+  {
+    url: '/about-us/',
+    name: 'About Us',
+  },
+  {
+    url: '/contact/',
+    name: 'Contact',
+  },
+];
+
+export {
+  navigationMenu,
+};
